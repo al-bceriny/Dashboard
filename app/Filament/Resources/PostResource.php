99900af050d6dd3e94a932fba0a5cd0288a1eabb
ADNANOVICH <eng.adnanovich@gmail.com>
2025-07-27 17:10:42 +0300

    Initial commit

---
+++ app/Filament/Resources/PostResource.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PostResource\Pages;
+use App\Filament\Resources\PostResource\RelationManagers;
+use App\Models\Category;
+use App\Models\Post;
+use Faker\Guesser\Name;
+use Filament\Forms;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TagsInput;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\CheckboxColumn;
+use Filament\Tables\Columns\ColorColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Livewire\Attributes\Title;
+use Symfony\Contracts\Service\Attribute\Required;
+
+use function Laravel\Prompts\select;
+
+class PostResource extends Resource
+{
+    protected static ?string $model = Post::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+
+            ->schema([
+                Section::make('Create Post Adnan Bey') // عنوان وشرحه بالأسفل
+                ->description('Create Posts Over Here (It`s a Description)')
+                // ->aside() // تجعل العنوان بجانب العناصر
+                ->collapsible() // تسمح بطوي العناصر في سيكشن
+
+            ->schema([
+                    TextInput::make('title')->rules(['min:3','max:15'])->required(),
+                    TextInput::make('slug'),
+                    Select::make('category_id')
+                        ->label('Category')
+                        ->relationship('category','name'),
+
+                    ColorPicker::make('color')->required(),
+
+                    MarkdownEditor::make('content')->required()->columnSpanFull(),
+                    ])->columnSpan(2)->columns(2),
+                Group::make()->schema([
+                Section::make("Image")
+                ->collapsible()
+            ->schema([
+                    FileUpload::make('thumbnail')->disk('public')->directory('thumbnails'),
+                    ])->columnSpan(1),
+                Section::make('Meta')->schema([
+                    TagsInput::make('tags')->required(),
+                    Checkbox::make('published'),
+                    ])->collapsible()
+                ]),
+            ])->columns(3);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')
+                ->sortable()
+                ->searchable()
+                ->toggleable(),
+                TextColumn::make('category_id')
+                ->sortable()
+                ->searchable()
+                ->toggleable(isToggledHiddenByDefault:true),
+                TextColumn::make('category.name')
+                ->sortable()
+                ->searchable()
+                ->toggleable(),
+                TextColumn::make('title')
+                ->toggleable()
+                ->sortable()
+                ->searchable(),
+                TextColumn::make('slug'),
+                CheckboxColumn::make('published'),
+                ColorColumn::make('color'),
+                TextColumn::make('created_at')
+                ->label('Published on')
+                ->date()
+                ->sortable()
+                ->toggleable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPosts::route('/'),
+            'create' => Pages\CreatePost::route('/create'),
+            'edit' => Pages\EditPost::route('/{record}/edit'),
+        ];
+    }
+}
